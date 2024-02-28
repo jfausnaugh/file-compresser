9b27ace96c893a61e0e0d39e7dc25d586299a212
Juliette Fausnaugh <j.d.land.16@gmail.com>
2024-02-28 13:31:55 -0500

    changed the colors of gui

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from zip_creator import make_zip
 
 # Create widgets
 
-label1 = sg.Text("Select files to compress: ")
+label1 = sg.Text("Select files to compress: ",
+                 background_color="light gray", text_color="black")
 input1 = sg.Input()
 choose_button1 = sg.FilesBrowse("Choose", key="files")
 
-label2 = sg.Text("Select the destination folder:")
+label2 = sg.Text("Select the destination folder:",
+                 background_color="light gray", text_color="black")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
-output_label = sg.Text(key="output", text_color="green")
+output_label = sg.Text(key="output", text_color="green",
+                       background_color="light gray")
 
 # create window
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
-                           [compress_button, output_label]])
+                           [compress_button, output_label]],
+                   background_color="light gray", button_color="royal blue")
 
 while True:
     event, values = window.read()
     filepaths = values["files"].split(";")
+    print(values)
     folder = values["folder"]
     make_zip(filepaths, folder)
     window["output"].update(value="Compression completed!")
