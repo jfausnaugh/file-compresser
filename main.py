af2fc35aa55c361c6c377a6b848c9b02c1357985
Juliette Fausnaugh <j.d.land.16@gmail.com>
2024-02-28 12:58:53 -0500

    Create widgets and gui window

---
+++ main.py
@@ -0,0 +1,23 @@
+import PySimpleGui as sg
+
+# Create widgets
+
+label1 = sg.Text("Select files to compress: ")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose", key="files")
+
+label2 = sg.Text("Select the destination folder:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
+
+compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output", text_color="green")
+
+# create window
+
+window = sg.Window("File Compressor",
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [compress_button, output_label]])
+
+
